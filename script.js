61b173995e596a5163b11ab3a753a5b791d538a5
Pavnendra Singh Patel <pavnendra19@gmail.com>
2024-01-08 22:16:14 +0530

    Portfolio project

---
+++ script.js
@@ -0,0 +1,14 @@
+var tabLinks = document.querySelectorAll('.tab-links');
+var tabContents = document.querySelectorAll('.tab-contents');
+
+const openTab = (tabname) => {
+    for(tablink of tabLinks) {
+        tablink.classList.remove('active-link');
+    }
+    
+    for(tabcontent of tabContents) {
+        tabcontent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
